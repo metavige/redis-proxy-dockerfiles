6e35a0a15cadab47ee35d465a96d019d564e3b12
metavige <metavige@gmail.com>
2014-11-26 13:47:49 +0800

    add reconfigure.sh to image

---
+++ sentinel/reconfigure.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#!/bin/bash
+# <master-name> <role> <state> <from-ip> <from-port> <to-ip> <to-port>
+export PATH=/sbin:/usr/local/bin:$PATH
+MASTER_NAME=$1
+ROLE=$2
+STATE=$3
+FROM_IP=$4
+FROM_PORT=$5
+TO_IP=$6
+TO_PORT=$7
+
+# 如果有七個參數，才做以下的動作，表示真的有變更 master
+if [ "$#" = "7" ]; then
+	# 透過變更 ETCD 的資料來觸發 confd/twemproxy restart
+	curl -L "$ETCD_HOST/v2/keys/$PROCESS_ID/redis/A" -XPUT -d "value=$TO_IP:$TO_PORT"
+fi
--- twemproxy/start_proxy.sh
+++ twemproxy/start_proxy.sh
@@ -11,7 +11,7 @@ ETCD_HOST=$PUBLIC_IP:4001
 curl -L $ETCD_HOST/v2/keys/services/twemproxy/port -XPUT -d value=$TWEMPROXY_PORT
 # docker rm -f redis-twemproxy
 
-docker run -d -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6222 -e ETCD_HOST=$ETCD_HOST nebula/redis-twemproxy
+docker run -d -p 2222:22 -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6222 -e ETCD_HOST=$ETCD_HOST nebula/redis-twemproxy
 # etcdctl set /services/twemproxy/host 127.0.0.1
 curl -L $ETCD_HOST/v2/keys/services/twemproxy/host -XPUT -d value=$PUBLIC_IP
 
