ca9b85fe2d1ca981a5008d9f24604e8718ada2bb
metavige <metavige@gmail.com>
2014-11-26 14:40:09 +0800

    modify proxy, change detect etcd key value

--- twemproxy/run.sh
+++ twemproxy/run.sh
@@ -6,8 +6,13 @@ if [ -n "${ETCD_HOST:+x}" ]; then
   sed -e "/confd -node/s/127.0.0.1:4001/${ETCD_HOST}/" /tmp/twemproxy.conf > /etc/supervisor/conf.d/twemproxy.conf
 fi
 
-# for debugging
-cat /etc/supervisor/conf.d/twemproxy.conf
+if [ -n "${PROCESS_ID:+x}" ]; then
+  mv /etc/confd/conf.d/twemproxy.toml /tmp/twemproxy.toml
+  sed "s/services/$PROCESS_ID/g" /tmp/twemproxy.toml > /etc/confd/conf.d/twemproxy.toml
+
+  mv /etc/confd/templates/twemproxy.tmpl /tmp/twemproxy.tmpl
+  sed "s/services/$PROCESS_ID/g" /tmp/twemproxy.tmpl > /etc/confd/templates/twemproxy.tmpl
+fi
 
 # /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 /root/bin/run.sh
--- twemproxy/start_proxy.sh
+++ twemproxy/start_proxy.sh
@@ -8,10 +8,10 @@ ETCD_HOST=$PUBLIC_IP:4001
 
 #etcdctl set /services/twemproxy/port $TWEMPROXY_PORT
 # Call ETCD, to setting up TWEMPROXY_PORT
-curl -L $ETCD_HOST/v2/keys/services/twemproxy/port -XPUT -d value=$TWEMPROXY_PORT
+curl -L $ETCD_HOST/v2/keys/$1/twemproxy/port -XPUT -d value=$TWEMPROXY_PORT
 # docker rm -f redis-twemproxy
 
-docker run -d -p 2222:22 -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6222 -e ETCD_HOST=$ETCD_HOST nebula/redis-twemproxy
+docker run -d -p 2222:22 -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6222 -e ETCD_HOST=$ETCD_HOST -e PROCESS_ID=$1 nebula/redis-twemproxy
 # etcdctl set /services/twemproxy/host 127.0.0.1
-curl -L $ETCD_HOST/v2/keys/services/twemproxy/host -XPUT -d value=$PUBLIC_IP
+curl -L $ETCD_HOST/v2/keys/$1/twemproxy/host -XPUT -d value=$PUBLIC_IP
 
