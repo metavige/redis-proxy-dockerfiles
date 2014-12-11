33d18bfdf5e85daf67c11445e121fc174e4532e7
Ricky Chiang <metavige@gmail.com>
2014-12-11 20:49:05 +0800

    fix supervisorctl restart command bug

--- sentinel/reconfigure.sh
+++ sentinel/reconfigure.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#!/bin/bash
 # <master-name> <role> <state> <from-ip> <from-port> <to-ip> <to-port>
 export PATH=/sbin:/usr/local/bin:$PATH
 MASTER_NAME=$1
@@ -10,6 +9,8 @@ FROM_IP=$4
 FROM_PORT=$5
 TO_IP=$6
 TO_PORT=$7
+
+echo "Reconfigure MASTER: $MASTER_NAME to $TO_IP:$TO_PORT"
 
 # 如果有七個參數，才做以下的動作，表示真的有變更 master
 if [ "$#" = "7" ]; then
